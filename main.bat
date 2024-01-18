36e838d8b1cd36565c5e9ace52b22b4e2abd0b2d
Mefamex <akkocmehmetakif2002@gmail.com>
2024-01-19 01:01:53 +0300

    added

--- main.bat
+++ main.bat
@@ -1,1 +1,3 @@
-ffmpeg -i “[link]”-c:v libx264 -preset ultrafast -crf [1-30 (its for quality, 1 is the best qualty] [create video name with].mp4
+ffmpeg -i "[link]"-c:v libx264 -preset ultrafast -crf [1-30 (its for quality, 1 is the best quality] [create video name with].mp4
+
+:: for example ffmpeg -i https://example.site/720p.m3u8 -c:v libx264 -preset ultrafast -crf 1 720pfilm.mp4
